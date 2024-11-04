396810be161a18325c6bb9493547b31e59832d56
heecircle <yhw991228@gmail.com>
2024-11-04 12:36:41 +0900

    ✨ Feat: add domain for feed

--- src/main/java/com/heewon/sns/feed/domain/Feed.java
+++ src/main/java/com/heewon/sns/feed/domain/Feed.java
@@ -1,2 +1,28 @@
-package com.heewon.sns.feed.domain;public class Feed {
+package com.heewon.sns.feed.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Feed {
+
+	@Id
+	private Long id;
+
+	@Column
+	private String title;
+
+	@Column(length = 1024)
+	private String content;
+
+	@Column
+	private String author;
+
+	@Column
+	private LocalDateTime createTime;
+
+
 }
