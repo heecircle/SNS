a86a0edf2781f0d1e13c974e01d36b34ebc43091
heecircle <yhw991228@gmail.com>
2024-11-04 12:37:27 +0900

    ✨ Feat: add domain for user

--- src/main/java/com/heewon/sns/user/domain/User.java
+++ src/main/java/com/heewon/sns/user/domain/User.java
@@ -1,2 +1,24 @@
-package com.heewon.sns.user.domain;public class User {
+package com.heewon.sns.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	private Long id;
+
+	@Column(nullable = false)
+	private String userId;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	private String nickname;
+
+	@Column(nullable = false)
+	private String imgUrl;
+
 }
