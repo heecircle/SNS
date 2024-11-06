78ecb62e60784d45ba12a979d9c8efbae057527d
heecircle <yhw991228@gmail.com>
2024-11-06 17:19:10 +0900

    ✨ Feat: add feature for scheduler for get feed list

--- src/main/java/com/heewon/sns/scheduler/CheckScheduler.java
+++ src/main/java/com/heewon/sns/scheduler/CheckScheduler.java
@@ -1,2 +1,26 @@
-package com.heewon.sns.scheduler;public class CheckScheduler {
+package com.heewon.sns.scheduler;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.heewon.sns.feed.repository.FeedCheckRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class CheckScheduler {
+
+	private final FeedCheckRepository feedCheckRepository;
+
+	@Transactional
+	@Scheduled(cron = "0 * * * * *")
+	public void checkMinute() {
+		System.out.println(new Date().toString());
+		feedCheckRepository.deleteByCheckTimeBefore(LocalDateTime.now().minusMinutes(5));
+	}
 }
