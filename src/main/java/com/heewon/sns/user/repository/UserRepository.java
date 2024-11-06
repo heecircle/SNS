2f15fc53471fa796fa9a31c0db2e1a3c5ee04802
heecircle <yhw991228@gmail.com>
2024-11-06 17:14:31 +0900

    ✨ Feat: add user entity and feature

--- src/main/java/com/heewon/sns/user/repository/UserRepository.java
+++ src/main/java/com/heewon/sns/user/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package com.heewon.sns.user.repository;public class UserRepository {
+package com.heewon.sns.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.heewon.sns.user.domain.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	User findById(long id);
 }
